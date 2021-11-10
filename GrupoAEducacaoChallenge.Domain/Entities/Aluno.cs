740777afc844c19abab73a9f843b517af57c52ea
Lucas Lima <lucas.lima.pinheiro@hotmail.com>
2021-11-10 01:32:15 -0300

    Implementação da camada de domínio

---
+++ GrupoAEducacaoChallenge.Domain/Entities/Aluno.cs
@@ -0,0 +1,65 @@
+﻿using GrupoAEducacaoChallenge.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoAEducacaoChallenge.Domain
+{
+    public class Aluno
+    {
+        public int Id { get; private set; }
+        public string Email { get; private set; }
+        public string Name { get; private set; }
+        public int Ra { get; private set; }
+        public string CPF { get; private set; }
+
+        public Aluno(string email, string name, int ra, string cpf)
+        {
+            ValidateDomain(email, name, ra, cpf);
+        }       
+        
+        public Aluno(int id, string email, string name, int ra, string cpf)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+            Id = id;
+            ValidateDomain(email, name, ra, cpf);
+        }
+        public void Update(string email, string name, int ra, string cpf)
+        {
+            ValidateDomain(email, name, ra, cpf);
+        }
+        public void ValidateDomain(string email, string name, int ra, string cpf)
+        {
+            DomainExceptionValidation.When(IsValidEmail(email), "Invalid email.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(email), "Invalid email. Email is required.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required.");
+            DomainExceptionValidation.When(ra <= 0, "Invalid Ra. Ra can not be 0.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(cpf), "Invalid CPF. CPF is required.");
+
+            Email = email;
+            Name = name;
+            Ra = ra;
+            CPF = cpf;
+
+        }
+
+        bool IsValidEmail(string email)
+        {
+            if (email.Trim().EndsWith("."))
+            {
+                return false; // suggested by @TK-421
+            }
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ GrupoAEducacaoChallenge.Domain/Interfaces/IAlunoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoAEducacaoChallenge.Domain
+{
+    public interface IAlunoRepository
+    {
+        Task<IEnumerable<Aluno>> GetAlunosAsync();
+        Task<Aluno> GetByIdAsync(int? id);
+
+        Task<Aluno> CreateAsync(Aluno Aluno);
+        Task<Aluno> UpdateAsync(Aluno Aluno);
+        Task<Aluno> RemoveAsync(Aluno Aluno);
+    }
+}
