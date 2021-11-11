416779f58499426f11facb0fec955633f6f4276f
Lucas Lima <lucas.lima.pinheiro@hotmail.com>
2021-11-11 00:07:45 -0300

    Implementação de classe responsável pelo controle de injeção de dependências do projeto, na clase Infra IoC

---
+++ GrupoAEducacaoChallenge.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using GrupoAEducacaoChallenge.Domain;
+using GrupoAEducacaoChallenge.Infra.Data.Context;
+using GrupoAEducacaoChallenge.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoAEducacaoChallenge.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IAlunoRepository, AlunoRepository>();
+
+            return services;
+        }
+    }
+}
